4081454292162cc1ffc862f4e911f214f9c5fc3c
andela-iikikin <ichiato.ikikin@andela.com>
2017-04-04 17:11:03 +0100

    [Chore]Change variable name to more descriptive names

--- tests/test_app_models.py
+++ tests/test_app_models.py
@@ -13,12 +13,12 @@ class AppModelTestCase(unittest.TestCase):
         self.app_context = self.app.app_context()
         self.app_context.push()
         db.create_all()
-        self.u = User(first_name='seni', last_name='abdulahi',
-                      email='seni@andel.com', password='123456')
-        self.u2 = User(first_name='seni', last_name='abdulahi',
-                       email='seni2@andel.com', password='123456')
-        self.s = ShortUrl(short_url="nfdjf")
-        self.s2 = ShortUrl(short_url="hjf97")
+        self.user = User(first_name='seni', last_name='abdulahi',
+                         email='seni@andel.com', password='123456')
+        self.user2 = User(first_name='seni', last_name='abdulahi',
+                          email='seni2@andel.com', password='123456')
+        self.short_url = ShortUrl(short_url="nfdjf")
+        self.short_url2 = ShortUrl(short_url="hjf97")
         self.long_url = LongUrl(long_url=""
                                 "https://docs.python.org/3/contents.html")
         self.long_url2 = LongUrl(long_url="https://repl.it/languages/python3")
@@ -29,98 +29,102 @@ class AppModelTestCase(unittest.TestCase):
         self.app_context.pop()
 
     def test_password_setter(self):
-        self.assertIsNotNone(self.u.password_hash)
+        self.assertIsNotNone(self.user.password_hash)
 
     def test_no_password_getter(self):
         with self.assertRaises(AttributeError):
-            self.u.password
+            self.user.password
 
     def test_password_verification(self):
-        self.assertTrue(self.u.verify_password('123456'))
-        self.assertFalse(self.u.verify_password('password'))
+        self.assertTrue(self.user.verify_password('123456'))
+        self.assertFalse(self.user.verify_password('password'))
 
     def test_password_salts_are_random(self):
-        self.assertTrue(self.u.password_hash != self.u2.password_hash)
+        self.assertTrue(self.user.password_hash != self.user2.password_hash)
 
     def test_saving_null_to_users_table(self):
-        u = User(password="123456")
+        user = User(password="123456")
         with self.assertRaises(sqlalchemy.exc.IntegrityError):
-            db.session.add(u)
+            db.session.add(user)
             db.session.commit()
 
     def test_token_generation(self):
-        self.assertIsInstance(self.u.generate_auth_token(34), bytes)
+        self.assertIsInstance(self.user.generate_auth_token(34), bytes)
 
     def test_token_verification(self):
-        db.session.add(self.u, self.u2)
+        db.session.add(self.user, self.user2)
         db.session.commit()
-        u_token = self.u.generate_auth_token(1)
-        self.assertNotEqual(self.u.verify_auth_token(u_token), self.u2)
-        self.assertEqual(self.u.verify_auth_token(u_token), self.u)
+        user_token = self.user.generate_auth_token(1)
+        self.assertNotEqual(self.user.verify_auth_token(user_token),
+                            self.user2)
+        self.assertEqual(self.user.verify_auth_token(user_token), self.user)
         sleep(2)
-        self.assertIsNone(self.u.verify_auth_token(u_token))
-        self.assertIsNone(self.u.verify_auth_token('jdjdje230920093944334j'))
+        self.assertIsNone(self.user.verify_auth_token(user_token))
+        self.assertIsNone(self.
+                          user.verify_auth_token('jdjdje230920093944334j'))
 
     def test_shorturl_time_saving(self):
-        self.long_url.short_urls.append(self.s)
-        self.u.short_urls.append(self.s)
-        self.long_url.users.append(self.u)
-        db.session.add(self.s)
+        self.long_url.short_urls.append(self.short_url)
+        self.user.short_urls.append(self.short_url)
+        self.long_url.users.append(self.user)
+        db.session.add(self.short_url)
         db.session.commit()
-        self.assertIs(type(self.s.when), datetime)
+        self.assertIs(type(self.short_url.when), datetime)
 
     def test_saving_user_to_db(self):
-        self.u.save()
-        db_u = User.query.filter_by(email='seni@andel.com').first()
-        self.assertIs(self.u, db_u)
-        self.assertIsInstance(db_u, User)
+        self.user.save()
+        db_user = User.query.filter_by(email='seni@andel.com').first()
+        self.assertIs(self.user, db_user)
+        self.assertIsInstance(db_user, User)
 
     def test_shorturl_not_saving_without_longurl(self):
-        db.session.add(self.s)
+        db.session.add(self.short_url)
         with self.assertRaises(sqlalchemy.exc.IntegrityError):
             db.session.commit()
 
     def test_shorturl_is_active_defualts(self):
-        self.long_url.short_urls.append(self.s)
-        self.u.short_urls.append(self.s)
-        self.long_url.users.append(self.u)
-        db.session.add(self.s)
+        self.long_url.short_urls.append(self.short_url)
+        self.user.short_urls.append(self.short_url)
+        self.long_url.users.append(self.user)
+        db.session.add(self.short_url)
         db.session.commit()
-        self.assertTrue(self.s.is_active)
-        self.s.is_active = False
-        self.assertFalse(self.s.is_active)
+        self.assertTrue(self.short_url.is_active)
+        self.short_url.is_active = False
+        self.assertFalse(self.short_url.is_active)
 
     def test_shorturl_longurl_relationship(self):
-        self.long_url.short_urls.append(self.s)
-        self.long_url.short_urls.append(self.s2)
-        self.u.short_urls.append(self.s)
-        self.u.short_urls.append(self.s2)
-        self.long_url.users.append(self.u)
-        self.long_url2.short_urls.append(self.s)
-        self.long_url2.users.append(self.u)
-        db.session.add_all([self.u, self.s, self.s2, self.long_url,
-                            self.long_url2])
+        self.long_url.short_urls.append(self.short_url)
+        self.long_url.short_urls.append(self.short_url2)
+        self.user.short_urls.append(self.short_url)
+        self.user.short_urls.append(self.short_url2)
+        self.long_url.users.append(self.user)
+        self.long_url2.short_urls.append(self.short_url)
+        self.long_url2.users.append(self.user)
+        db.session.add_all([self.user, self.short_url, self.short_url2,
+                            self.long_url, self.long_url2])
         db.session.commit()
-        self.assertEqual(self.s.long_url_id, self.long_url2.id)
-        self.assertNotEqual(self.s.long_url_id, self.long_url.id)
-        self.assertEqual(self.s2.long_url_id, self.long_url.id)
+        self.assertEqual(self.short_url.long_url_id, self.long_url2.id)
+        self.assertNotEqual(self.short_url.long_url_id, self.long_url.id)
+        self.assertEqual(self.short_url2.long_url_id, self.long_url.id)
 
     def test_shorturl_user_relationship(self):
-        self.u.short_urls.append(self.s)
-        self.u2.short_urls.append(self.s)
-        self.u.short_urls.append(self.s2)
-        self.long_url.short_urls.append(self.s)
-        self.long_url.short_urls.append(self.s2)
-        db.session.add_all([self.u, self.u2, self.s, self.s2])
+        self.user.short_urls.append(self.short_url)
+        self.user2.short_urls.append(self.short_url)
+        self.user.short_urls.append(self.short_url2)
+        self.long_url.short_urls.append(self.short_url)
+        self.long_url.short_urls.append(self.short_url2)
+        db.session.add_all([self.user, self.user2, self.short_url,
+                            self.short_url2])
         db.session.commit()
-        self.assertEqual(self.s.user_id, self.u2.id)
-        self.assertNotEqual(self.s.user_id, self.u.id)
-        self.assertEqual(self.s2.user_id, self.u.id)
+        self.assertEqual(self.short_url.user_id, self.user2.id)
+        self.assertNotEqual(self.short_url.user_id, self.user.id)
+        self.assertEqual(self.short_url2.user_id, self.user.id)
 
     def test_longurl_user_relationship(self):
-        self.long_url.users.append(self.u)
-        self.long_url.users.append(self.u2)
-        self.u.long_urls.append(self.long_url2)
-        db.session.add_all([self.long_url, self.long_url2, self.u, self.u2])
-        self.assertEqual(self.long_url.users, [self.u, self.u2])
-        self.assertEqual(self.u.long_urls, [self.long_url, self.long_url2])
+        self.long_url.users.append(self.user)
+        self.long_url.users.append(self.user2)
+        self.user.long_urls.append(self.long_url2)
+        db.session.add_all([self.long_url, self.long_url2, self.user,
+                            self.user2])
+        self.assertEqual(self.long_url.users, [self.user, self.user2])
+        self.assertEqual(self.user.long_urls, [self.long_url, self.long_url2])
