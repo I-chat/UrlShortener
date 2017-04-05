2776208b2dd9d9e9867d8199111213da352c2c63
andela-iikikin <ichiato.ikikin@andela.com>
2017-04-05 21:20:36 +0100

    [Feature]Setup api blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 from config import config
 from flask_login import LoginManager
 
 
+bootstrap = Bootstrap()
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_message = u'Please login.'
+login_manager.login_view = 'auth.login'
 
 
 def create_app(config_name):
@@ -12,6 +18,10 @@ def create_app(config_name):
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
     db.init_app(app)
+    bootstrap.init_app(app)
     from .main import main as main_blueprint
+    from .api import api as api_blueprint
     app.register_blueprint(main_blueprint)
+    app.register_blueprint(api_blueprint, url_prefix='/api')
+    login_manager.init_app(app)
     return app
