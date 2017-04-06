f99b540d9ae1c57d495b4fb54e69442c17c74d8b
andela-iikikin <ichiato.ikikin@andela.com>
2017-04-06 10:24:18 +0100

    [Chore]Remove redundant shell context

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from config import config
@@ -16,7 +16,7 @@ login_manager.login_view = 'auth.login'
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
-    config[config_name].init_app(app)
+    config[config_name].__init__(app)
     db.init_app(app)
     bootstrap.init_app(app)
     from .main import main as main_blueprint
