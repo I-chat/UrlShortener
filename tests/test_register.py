cbcbe236dc5f32e67e4d9e53e6367b12699385e3
andela-iikikin <ichiato.ikikin@andela.com>
2017-04-06 14:50:03 +0100

    [Chore]Add more tests for sad paths

--- tests/test_register.py
+++ tests/test_register.py
@@ -6,9 +6,9 @@ from flask import current_app, url_for
 from flask_sqlalchemy import sqlalchemy
 
 
-class ApiAuthentication(unittest.TestCase):
+class ApiRegistration(unittest.TestCase):
     def setUp(self):
-        self.app = create_app('testing')
+        self.app = create_app("testing")
         self.app_context = self.app.app_context()
         self.app_context.push()
         db.create_all()
@@ -21,8 +21,8 @@ class ApiAuthentication(unittest.TestCase):
 
     def content_type_header(self):
         return {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
+            "Accept": "application/json",
+            "Content-Type": "application/json"
         }
 
     def test_basic_register(self):
@@ -30,15 +30,15 @@ class ApiAuthentication(unittest.TestCase):
                 "firstname": "ichiato",
                 "lastname": "ikikin",
                 "email": "ichat@yahoo.com",
-                "password": '123456',
+                "password": "123456",
                 "confirm_password": "123456",
         })
         header = self.content_type_header()
-        response = self.client.post(url_for('api.new_user'), headers=header,
+        response = self.client.post(url_for("api.new_user"), headers=header,
                                     data=post_data)
-        json_response = json.loads(response.data.decode('utf-8'))
-        msg = json_response['message']
-        email = json_response['email']
+        json_response = json.loads(response.data.decode("utf-8"))
+        msg = json_response["message"]
+        email = json_response["email"]
         self.assertEqual(response.status_code, 201)
         self.assertEqual(msg, "Registration Successful.")
         self.assertEqual(email, "ichat@yahoo.com")
@@ -48,17 +48,17 @@ class ApiAuthentication(unittest.TestCase):
                 "firstname": "ichiato",
                 "lastname": "ikikin",
                 "email": "ichat@yahoo.com",
-                "password": '123456',
+                "password": "123456",
                 "confirm_password": "12356",
         })
         header = self.content_type_header()
-        response = self.client.post(url_for('api.new_user'), headers=header,
+        response = self.client.post(url_for("api.new_user"), headers=header,
                                     data=post_data)
-        json_response = json.loads(response.data.decode('utf-8'))
-        msg = json_response['message']
+        json_response = json.loads(response.data.decode("utf-8"))
+        msg = json_response["message"]
         self.assertEqual(response.status_code, 400)
-        self.assertEqual(msg, 'The request is invalid or inconsistent.'
-                         ' password and confirm_password do not match.')
+        self.assertEqual(msg, "The request is invalid or inconsistent."
+                         " password and confirm_password do not match.")
 
     def test_register_twice(self):
         # save a user first like he already registered
@@ -69,30 +69,82 @@ class ApiAuthentication(unittest.TestCase):
                 "firstname": "ichiato",
                 "lastname": "ikikin",
                 "email": "ichat@yahoo.com",
-                "password": '123456',
+                "password": "123456",
                 "confirm_password": "123456",
         })
         header = self.content_type_header()
-        response = self.client.post(url_for('api.new_user'), headers=header,
+        response = self.client.post(url_for("api.new_user"), headers=header,
                                     data=post_data)
-        json_response = json.loads(response.data.decode('utf-8'))
-        msg = json_response['message']
+        json_response = json.loads(response.data.decode("utf-8"))
+        msg = json_response["message"]
         self.assertEqual(response.status_code, 400)
-        self.assertEqual(msg, 'The request is invalid or inconsistent.'
-                         ' User already registered.Try login')
+        self.assertEqual(msg, "The request is invalid or inconsistent."
+                         " User already registered.Try login.")
 
     def test_register_with_missing_key(self):
         post_data = json.dumps({
                 "firstname": "ichiato",
                 "email": "ichat@yahoo.com",
-                "password": '123456',
+                "password": "123456",
                 "confirm_password": "123456",
         })
         header = self.content_type_header()
-        response = self.client.post(url_for('api.new_user'), headers=header,
+        response = self.client.post(url_for("api.new_user"), headers=header,
                                     data=post_data)
-        json_response = json.loads(response.data.decode('utf-8'))
-        msg = json_response['message']
-        self.assertEqual(msg, 'The request is invalid or inconsistent.'
-                         ' Incomplete number of reqired keys provided.')
+        json_response = json.loads(response.data.decode("utf-8"))
+        msg = json_response["message"]
+        self.assertEqual(msg, "The request is invalid or inconsistent."
+                         " Incomplete number of required keys provided.")
         self.assertEqual(response.status_code, 400)
+
+    def test_register_with_bad_email(self):
+        post_data = json.dumps({
+                "firstname": "ichiato",
+                "lastname": "ikikin",
+                "email": "ichatyahoo.com",
+                "password": "123456",
+                "confirm_password": "123456",
+        })
+        header = self.content_type_header()
+        response = self.client.post(url_for("api.new_user"), headers=header,
+                                    data=post_data)
+        json_response = json.loads(response.data.decode("utf-8"))
+        msg = json_response["message"]
+        self.assertEqual(msg, "The request is invalid or inconsistent."
+                         " Not a valid email format.")
+        self.assertEqual(response.status_code, 400)
+
+    def test_register_with_int_password(self):
+        post_data = json.dumps({
+                "firstname": "ichiato",
+                "lastname": "ikikin",
+                "email": "ichat@yahoo.com",
+                "password": "123456",
+                "confirm_password": 123456,
+        })
+        header = self.content_type_header()
+        response = self.client.post(url_for("api.new_user"), headers=header,
+                                    data=post_data)
+        json_response = json.loads(response.data.decode("utf-8"))
+        msg = json_response["message"]
+        self.assertEqual(msg, "The request is invalid or inconsistent. Invalid"
+                         " password format. Passwords can only be strings.")
+        self.assertEqual(response.status_code, 400)
+
+    def test_register_with_int_name(self):
+        post_data = json.dumps({
+                "firstname": "ich34ato",
+                "lastname": "ikikin",
+                "email": "ichat@yahoo.com",
+                "password": "123456",
+                "confirm_password": "123456",
+        })
+        header = self.content_type_header()
+        response = self.client.post(url_for("api.new_user"), headers=header,
+                                    data=post_data)
+        json_response = json.loads(response.data.decode("utf-8"))
+        msg = json_response["message"]
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(msg, "The request is invalid or inconsistent. Invalid"
+                         " name format. Name should include only one or more"
+                         " alphabets.")
