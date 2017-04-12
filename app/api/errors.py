b27a13a1f85f33e5ac11ce929f9e6e7bd54ff038
andela-iikikin <ichiato.ikikin@andela.com>
2017-04-12 17:43:42 +0100

    [Chore]Change error message to be more explanatory

--- app/api/errors.py
+++ app/api/errors.py
@@ -6,7 +6,7 @@ from flask import request, render_template, jsonify
 @api.app_errorhandler(404)
 def page_not_found(e):
     if request.accept_mimetypes.accept_json:
-        response = jsonify({'error': 'not found'})
+        response = jsonify({'error': 'Resource not found'})
         response.status_code = 404
         return response
     return render_template('404.html'), 404
